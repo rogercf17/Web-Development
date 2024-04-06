e24cdf2938b1273cfda49b6687a29c2435501973
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-05 22:39:59 -0300

    Upload pasta Aula01 de Web Dev

---
+++ Aula01/script.js
@@ -0,0 +1,28 @@
+// Comentário de 1 linha
+
+/*
+Comentário em bloco
+*/
+
+// alert('Olá, mundo!');
+// prompt('Digite seu nome:');
+// confirm('Você deseja sair?');
+
+// Console
+// console.log('Roger')
+// console.warn('Atenção')
+// console.error('Erro')
+// console.info('Informação')
+
+// Variável
+// var nome = 'Roger';
+// var idade = 19
+// console.log(nome+idade+'anos');
+// console.log(`Olá ${nome}, seja bem vindo!`)
+
+// Exercício 1Roger
+var nome = prompt('Qual o seu nome?');
+var idade = prompt('Qual a sua idade?');
+var profissao = prompt('Qual é sua profissão?');
+alert(`Olá ${nome}, você têm ${idade} anos e trabalha como ${profissao}`);
+console.log(`Olá ${nome}, você têm ${idade} anos e trabalha como ${profissao}`);
