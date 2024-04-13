1791d4fed86f7d4342f20d35bd819010d3b10573
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-12 22:30:29 -0300

    Aula 02 projeto da calculadora

---
+++ Aula02 - Calculadora/main.js
@@ -0,0 +1,40 @@
+var resultado = document.getElementById('resultado');
+
+function somar() {
+    // Entrada
+    var numero1 = parseFloat(document.getElementById('numero1').value);
+    var numero2 = parseFloat(document.getElementById('numero2').value);
+
+    // Processamento
+    var soma = numero1 + numero2;
+
+    // Saída
+    resultado.innerText = `${numero1} + ${numero2} = ${soma}`;
+}
+
+function subtrair() {
+    var numero1 = parseFloat(document.getElementById('numero1').value);
+    var numero2 = parseFloat(document.getElementById('numero2').value);
+
+    var subtrair = numero1 - numero2;
+
+    resultado.innerText = `${numero1} - ${numero2} = ${subtrair}`;
+}
+
+function multiplicar() {
+    var numero1 = parseFloat(document.getElementById('numero1').value);
+    var numero2 = parseFloat(document.getElementById('numero2').value);
+
+    var multiplicar = numero1 * numero2;
+
+    resultado.innerText = `${numero1} x ${numero2} = ${multiplicar}`;
+}
+
+function dividir() {
+    var numero1 = parseFloat(document.getElementById('numero1').value);
+    var numero2 = parseFloat(document.getElementById('numero2').value);
+
+    var dividir = numero1 / numero2;
+
+    resultado.innerText = `${numero1} / ${numero2} = ${dividir}`;
+}
