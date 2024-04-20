9a6673be75fcaefee7f097e0ee08cf67cdb86657
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-19 22:40:41 -0300

    Arquivos atualizado Aula 03

--- Aula 03/script.js
+++ Aula 03/script.js
@@ -6,14 +6,23 @@
 //     console.log(`${numero} é maior que 0.`)
 // }
 // (numero < 0) ? console.log(`${numero} é menor que 0.`) : console.log(`${numero} é maior que 0.`)
-var resultado = document.getElementById("resultado");
-function verifica_velocidade() {
-    var velocidade = parseInt(document.getElementById("velocidade").value);
-    var multa = 100;
-    if (velocidade <= 60) {
-        resultado.innerHTML = `Velocidade = ${velocidade}Km, sem multa!`;
-    }else {
-        multa *= (velocidade - 60);
-        resultado.innerText =  `Velocidade = ${velocidade}Km, acima do valor permitido multa de R$ ${multa}`; 
-    }
+// var resultado = document.getElementById("resultado");
+// function verifica_velocidade() {
+//     var velocidade = parseInt(document.getElementById("velocidade").value);
+//     var multa = 100;
+//     if (velocidade <= 60) {
+//         resultado.innerHTML = `Velocidade = ${velocidade}Km, sem multa!`;
+//     }else {
+//         multa *= (velocidade - 60);
+//         resultado.innerText =  `Velocidade = ${velocidade}Km, acima do valor permitido multa de R$ ${multa}`; 
+//     }
+// }
+let item = 'Queijo';
+switch(item) {
+    case 'Pão':
+        console.log('é pão.');
+        break;
+    case 'Queijo':
+        console.log('é queijo.');
+        break;
 }
