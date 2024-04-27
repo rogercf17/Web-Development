cb855c2e572f27d32959131ce1bd41915a9eeb44
rogercf17 <rogercf16@gmail.com>
2024-04-26 22:46:34 -0300

    Enviando arquivos aula04

---
+++ Aula04/script.js
@@ -0,0 +1,38 @@
+// While
+// function calcular() {
+//     var numero = parseInt(document.getElementById("numero").value);
+//     let i = numero;
+
+//     while (i >= 0){
+//         console.log(i);
+//         i--;
+//     }
+// }
+// function calcular() {
+//     var numero = parseInt(document.getElementById("numero").value);
+//     let i = 0;
+
+//     while (numero >= i) {
+//         console.log(numero);
+//         numero --;
+//     }
+// }
+
+// For
+// for (let c = 0; c <= 10; c++) {
+//     console.log(c);
+// }
+// console.log("\n")
+// for (let c = 0; c <= 10; c+=2) {
+//     console.log(c);
+// }
+// function calcular() {
+//     var numero = parseInt(document.getElementById("numero").value);
+//     console.log(`Divisores de ${numero}`)
+//     for (let i = 1; i < numero; i++) {
+//         if (numero % i == 0){
+//             console.log(i);
+//         }
+//     }
+//     console.log("\n");
+// }
